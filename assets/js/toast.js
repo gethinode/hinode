9eb3eb45d823e736207eb894fa7fff68389254f4
mark <mark@dumay.nl>
2023-01-01 09:40:35 +0100

    Bundle primary scripts

---
+++ assets/js/toast.js
@@ -0,0 +1,10 @@
+// Bootstrap toast example: https://getbootstrap.com/docs/5.2/components/toasts/
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
---
+++ assets/js/tooltip.js
@@ -0,0 +1,3 @@
+// Bootstrap tooltip example: https://getbootstrap.com/docs/5.2/components/tooltips/
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
