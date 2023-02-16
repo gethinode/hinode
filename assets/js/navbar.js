ca511302867f0ee2e4f68e13346add599525af96
mark <mark@dumay.nl>
2023-02-16 09:36:19 +0100

    Fix linting issues

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,16 +1,16 @@
-const navbar = document.querySelector('.navbar');
-const toggler = document.getElementById('main-nav-toggler');
+const navbar = document.querySelector('.navbar')
+const toggler = document.getElementById('main-nav-toggler')
 
 // set the navbar background color to opaque when scrolling past a breakpoint
 window.onscroll = () => {
   if (window.scrollY > 75) {
-    navbar.classList.add('nav-active');
+    navbar.classList.add('nav-active')
   } else {
-    navbar.classList.remove('nav-active');
+    navbar.classList.remove('nav-active')
   }
 }
 
 // set the navbar background color to opaque when expanded
 toggler.onclick = () => {
-  navbar.classList.toggle('navbar-expanded');
+  navbar.classList.toggle('navbar-expanded')
 }
