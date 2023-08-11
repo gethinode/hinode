f80241548a39bf3b0fa741e638e92ce7bea583c3
mark <mark@dumay.nl>
2023-08-11 16:25:49 +0200

    Drop navbar button id to avoid potential clash

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,8 +1,8 @@
 const navbar = document.querySelector('.navbar')
-const toggler = document.getElementById('main-nav-toggler')
+const togglers = document.querySelectorAll('.main-nav-toggler')
 const modeSelectors = document.querySelectorAll('.switch-mode-collapsed')
 
-if (navbar !== null && toggler !== null) {
+if (navbar !== null && togglers !== null) {
   // set the navbar background color to opaque when scrolling past a breakpoint
   window.onscroll = () => {
     if (window.scrollY > 75) {
@@ -13,14 +13,21 @@ if (navbar !== null && toggler !== null) {
   }
 
   // set the navbar background color to opaque when expanded
-  toggler.onclick = () => {
-    navbar.classList.toggle('navbar-expanded')
+  for (let i = 0; i < togglers.length; ++i) {
+    togglers[i].onclick = () => {
+      navbar.classList.toggle('navbar-expanded')
+    }
   }
 
   // invoke the navbar toggler for each mode switcher to collapse the main menu afterwards
   for (let i = 0; i < modeSelectors.length; ++i) {
     modeSelectors[i].onclick = () => {
-      toggler.click()
+      for (let j = 0; j < togglers.length; ++j) {
+        const toggler = togglers[j]
+        if (toggler.getAttribute('aria-expanded') === 'true') {
+          toggler.click()
+        }
+      }
     }
   }
 }
